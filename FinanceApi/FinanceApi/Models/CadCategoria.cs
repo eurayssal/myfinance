fb27f3d5e4c800a92a198f1b4a51337dda2f7ecd
eurayssal <eurayssamagalhaes@gmail.com>
2024-02-06 15:17:58 -0300

    Front

--- FinanceApi/FinanceApi/Models/CadCategoria.cs
+++ FinanceApi/FinanceApi/Models/CadCategoria.cs
@@ -6,11 +6,17 @@ namespace FinanceApi.Models
 {
     public class CadCategoria
     {
+        public enum TipoCategoriaEnum
+        {
+            Receita = 0,
+            Despesa = 1
+        }
+
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string? NomeCategoria { get; set; }
-        public string TipoCategoria { get; set; } // Tipos: Receita e Despesa
+        public TipoCategoriaEnum? TipoCategoria { get; set; } // Tipos: Receita e Despesa
         public bool AtividadeCategoria { get; set; }
 
     }
